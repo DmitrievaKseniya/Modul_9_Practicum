e7ce04dd40c9e7d75a7dd3a7b6761c59d018f254
DmitrievaKseniya <dkn1933@mail.ru>
2023-01-25 19:43:04 +0400

    Добавьте файлы проекта.

---
+++ Modul_9_Practicum_1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Exception[] arrException = { new ArgumentException(), new FormatException(), new MyException("тест моего исключения"), new NotSupportedException(), new TimeoutException() };
+
+            foreach (Exception ex in arrException)
+            {
+                try
+                {
+                    throw ex;
+                }
+                catch
+                {
+                    Console.WriteLine($"Выведено исключение: {ex.GetType()} тест: {ex.Message}");
+                }
+            }
+        }
+    }
+
+    public class MyException : ArgumentException
+    { 
+        public MyException() 
+        {
+
+        }
+
+        public MyException(string message) : base(message) 
+        {
+
+        }
+    }
+}
